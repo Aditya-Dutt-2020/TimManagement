4e77b428236b9959893f1476d4904493a8dc2271
Reva Amritkar <revamamritkar@gmail.com>
2023-02-19 16:00:17 -0500

    more stuff

--- flask stuff/app.py
+++ flask stuff/app.py
@@ -2,8 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
+
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        if request.form.get('action1') == "VALUE1":
+            pass
+        elif request.form.get("action2") == "VALUE2":
+            pass
+        else:
+            pass
+    
+    elif request.method == "GET":
+        return render_template('index.html', form=form)
+
+    return render_template("index.html")
 
 #app.run('0.0.0.0', port=81, debug=True)
