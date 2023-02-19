85754f9a48e32b535138ef38d96d2b52a0c50f18
adimachinelearning <duttaditya@gmail.com>
2023-02-19 08:38:21 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world testing git")
+import tkinter as tk
+import time
+import random
+from random import randint
+from win32api import GetMonitorInfo, MonitorFromPoint
+
+monitor_info=GetMonitorInfo(MonitorFromPoint((0, 0)))
+work_area=monitor_info.get('Work')
+screen_width=work_area[2]
+work_height=work_area[3]
