d3e3227bd20d99abfa6f3968dcbc0303a81719cb
adimachinelearning <duttaditya@gmail.com>
2023-02-19 14:06:08 -0500

    nothing

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ work_height=work_area[3]
 
 idle = range(1,11)
 heart = range(12,15)
-logTime = range(16,25)
+walk = range(16,25)
+log = range(26,28)
 class Timmy:
     def __init__(self):
         self.window = tk.Tk()
@@ -31,12 +32,12 @@ class Timmy:
         self.walk = []
         for x in range(0,2):
             img = Image.open(f'Assets/tim/tim_walking/sprite_{str(x).zfill(1)}.png')
-            self.heart.append(ImageTk.PhotoImage(Image.merge('RGBA', [b.resize((250,250), Image.LINEAR) for b in img.split()])))
+            self.walk.append(ImageTk.PhotoImage(Image.merge('RGBA', [b.resize((250,250), Image.LINEAR) for b in img.split()])))
 
         self.logWalk = []
         for x in range(1,3):
             img = Image.open(f'Assets/tim/tim_log/tim_log{str(x).zfill(1)}.png')
-            self.heart.append(ImageTk.PhotoImage(Image.merge('RGBA', [b.resize((250,250), Image.LINEAR) for b in img.split()])))
+            self.logWalk.append(ImageTk.PhotoImage(Image.merge('RGBA', [b.resize((300,300), Image.LINEAR) for b in img.split()])))
 
         self.xPos=int(screen_width*0.8)
         self.yPos=work_height-130
@@ -61,11 +62,19 @@ class Timmy:
         self.initFrame = 0
         self.doingSomething = True
         self.eventNumber = 17
+        self.window.after(6000, self.transToLog)
 
     def pet(self,event):
         self.initFrame = 0
         self.doingSomething = True
         self.eventNumber = 12
+
+    def transToLog(self):
+        print("transed")
+        self.initFrame = 0
+        self.eventNumber = 26
+        
+
     def event(self, initFrame, state, eventNumber, xPos):        
         if self.eventNumber in idle and not self.doingSomething:
             self.state = 1
@@ -75,10 +84,14 @@ class Timmy:
             self.state = 2
             self.yPos = work_height-self.frame.height()
             self.window.after(300, self.updateFrame, self.initFrame, self.state, self.eventNumber, self.xPos)
-        if self.eventNumber in logTime:
+        if self.eventNumber in walk:
             self.state = 3
             self.yPos = work_height-self.frame.height()
-            self.window.after(100, self.updateFrame, self.initFrame, self.state, self.eventNumber, self.xPos)
+            self.window.after(200, self.updateFrame, self.initFrame, self.state, self.eventNumber, self.xPos)
+        if self.eventNumber in log:
+            self.state = 4
+            self.yPos = work_height-self.frame.height()
+            self.window.after(200, self.updateFrame, self.initFrame, self.state, self.eventNumber, self.xPos)
             
     def animate(self, initFrame, animation, eventNumber, x, y):
         if self.initFrame<len(animation)-1:
@@ -87,7 +100,12 @@ class Timmy:
             if not(eventNumber in idle):
                 self.doingSomething = False
             self.initFrame = 0
-            self.eventNumber=randint(x,y)
+            if self.eventNumber not in [17,26]:
+                self.eventNumber=1
+        if self.eventNumber==17 and self.xPos <= screen_width:
+            self.xPos+=10
+        if self.eventNumber==26 and self.xPos >= int(screen_width*0.8):
+            self.xPos-=10
         return self.initFrame, self.eventNumber
     
     def updateFrame(self, initFrame, state, eventNumber, xPos):
@@ -98,6 +116,12 @@ class Timmy:
         if state == 2:
             self.frame = self.heart[self.initFrame]
             self.initFrame, self.eventNumber=self.animate(self.initFrame, self.heart, self.eventNumber, 1, 1)
+        if state == 3:
+            self.frame = self.walk[self.initFrame]
+            self.initFrame, self.eventNumber=self.animate(self.initFrame, self.walk, self.eventNumber, 1,1)
+        if state == 4:
+            self.frame = self.logWalk[self.initFrame]
+            self.initFrame, self.eventNumber = self.animate(self.initFrame, self.logWalk, self.eventNumber, 1,1)
         self.window.geometry(f'{self.frame.width()}x{self.frame.height()}+'+str(self.xPos)+'+'+str(self.yPos))
         #self.window.geometry('72x64+'+str(self.xPos)+'+'+str(self.yPos))
         self.label.configure(image=self.frame)
