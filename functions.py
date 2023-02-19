1813ee41efc1d850b680895c9bcb32ffbae900e6
Reva Amritkar <revamamritkar@gmail.com>
2023-02-19 13:44:17 -0500

    api ;-;

---
+++ functions.py
@@ -0,0 +1,13 @@
+from apiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+scopes = []
+
+def event_detector():
+    print("Hello")
+    return
+
+def time_difference():
+    print("Hello")
+    return
+
